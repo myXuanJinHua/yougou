5e4fc3bd876c1363bc0ec294aa796f9f9022ac36
hua <a6927561@163.com>
2019-08-17 15:40:26 +0800

    做完分类首页

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,3 +1,4 @@
+import request from "../../utils/request.js"
 // pages/category/index.js
 Page({
 
@@ -5,62 +6,31 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    // 大分类
+    cates:[],
+    // 
+    current:0,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad() {
+    request({
+      url:"/categories"
+    }).then(res=>{
+      console.log(res)
+      let {message}=res.data
+      this.setData({
+        cates:message
+      })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleTap(event){
+    // console.log(event)
+    let { index } = event.currentTarget.dataset
+    this.setData({
+      current:index
+    })
   }
 })
